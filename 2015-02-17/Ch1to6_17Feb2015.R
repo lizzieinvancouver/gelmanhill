93bfed42536a1b91560d7b42196835cfc94982df
AileneKane <aettinger@fasmail.harvard.edu>
2015-02-17 12:57:55 -0500

    Update Ch1to6_17Feb2015.R
    updating the code, adding some explanations of what different examples are demonstrating.

--- 2015-02-17/Ch1to6_17Feb2015.R
+++ 2015-02-17/Ch1to6_17Feb2015.R
@@ -40,39 +40,41 @@ anova(indmod)
 indmod_noint<-lm(rwi~-1+ind)#remove interecept so that you can see the coefficients for each individual
 summary(indmod_noint)#
 
-##Ch5: "Logistic regression" (basically, binomial glms), using Ailene's tree transplant data frmo Seattle. WC=Woodchips present (0/1=No/Yes), Ivy=Ivy present (0/1=No/Yes)
+##Ch5: "Logistic regression" (basically, binomial glms, but when all responses are either 0 or 1), using Ailene's tree transplant data frmo Seattle. WC=Woodchips present (0/1=No/Yes), Ivy=Ivy present (0/1=No/Yes)
 urbantrees=read.csv("urbantrees.csv", header=T)
 urbantrees
-#does ivy have an affect on 
+#does ivy have an affect on survival?
 quartz()
 plot(urbantrees$status ~ urbantrees$Ivy)
 fit.1 <- glm (status ~ Ivy, family=binomial(link="logit"), data=urbantrees)
 summary(fit.1)
-coef(fit.1)
-invlogit(coef(fit.1))
+coef(fit.1)#these are the estimates for survival on a logit scale
+invlogit(coef(fit.1))#this gives the survival rates for no ivy (the "intercept" term), which = 0.44 and with ivy present, which = 0.44). Thus we conclude that ivy does not singificanylt affect survival
+
 
 ##Ch6: Generalized linear models (glms)
-#binomial
+#binomial, which have response variables that consist of two columns- successes and failures
 birds = read.csv("BankSwallows.csv")
 birds
 attach(birds)
-colFates = cbind(num_extinct, (num_colonies)-(num_extinct))#"successes & failures"
+colFates = cbind(num_extinct, (num_colonies)-(num_extinct))#"successes & failures" (in this case, "successess" are the number of colunies that went extinct)
 m.same.logit = glm(colFates ~ 1, family = binomial(link = "logit"))
-summary(m.same.logit)#coef=0.39058, inv.logit(0.39058)=0.5964223
-invlogit(coef(m.same.logit))
+summary(m.same.logit)
+invlogit(coef(m.same.logit))#coef=0.39058, inv.logit(0.39058)=0.5964223, so this is the probability of extinction
 logLik(m.same.logit)
-m.time.logit = glm(colFates ~ -1 + period, family = binomial(link = "logit"))
+#now, let's test if extinction rate differs from early to late season (period = early or late)
+m.time.logit = glm(colFates ~ -1 + period, family = binomial(link = "logit"))#remove the intercept to allow for easier interpretation of coefficients, which an be interpreted as the probability of extinction early vs. late season
 summary(m.time.logit)
 coef(m.time.logit)
-inv.logit(coef(m.time.logit))
+inv.logit(coef(m.time.logit))#periodearly= 0.4479638m, periodlate =0.7127660, so extinction rate is higher late than early
 
 #compare two models
-logLik(m.time.logit,m.same.logit)
+logLik(m.time.logit)
+logLik(m.same.logit)
 AIC(m.time.logit,m.same.logit)
-anova(m.time.logit, test="Chi")
+anova(m.time.logit, test="Chi")#including period provides better fit (lower AIC and higher likelihood)
 
-#Effect of using a different link function
-
+#Effect of using a different link function; the identity function avoids the need to backtransform estimates to interpret them, but is not appropriate when continuous predictors are included.
 m.same.ID = glm(colFates ~ 1, family = binomial(link = "identity"))
 summary(m.same.ID)#coef=0.59642 
 #logLik(m.same.ID)
@@ -89,16 +91,16 @@ summary(m.same.ID)#coef=0.59642
 #logLik(m.same.ID)
 #lower AIC for time model (also higher likelihood for time model), so this one is better fit
 
-###Poisson, using same dataset on bak swallows
+###Poisson, using same dataset on bank swallows
 #Test whether the actual number of colonies different between early and late years (Are the birds leaving the system or dying, s just abandoning burrows more often?)
 birds
 m.numcols = glm(num_colonies ~ 1 , family = poisson (link = "log"))
 summary(m.numcols)
+exp(coef(m.numcols))#estimate of mean number of colonies across all periods=~39
 m.numcols.time = glm(num_colonies ~ period , family = poisson)
-summary(m.numcols.time)
+summary(m.numcols.time)#does number of colonies differ from early to late?
 AIC(m.numcols,m.numcols.time)
-anova(m.numcols,m.numcols.time, test="Chi")
+anova(m.numcols,m.numcols.time, test="Chi")#no difference betwee models, and AIC is slightly higher with period included, so number of colonies does not seem to differ significantly frmo early to late period
 coef(m.numcols.time)
 exp(coef(m.numcols.time))#36.83321 colonies on avg early
-exp(sum(coef(m.numcols.time)))#40.28544 on average late
-
+exp(sum(coef(m.numcols.time)))#40.28544 on average late, but apparently these are not significantly different, given AIC, likelihood, etc.
