fa1b72bc3a813c4904b091116fdf130ac68a6327
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2016-05-17 15:00:29 -0400

    Update Bayes PGLS code, runs in R!

--- BayesPGLS/pgls.R
+++ BayesPGLS/pgls.R
@@ -2,23 +2,41 @@
 # https://github.com/Auerilas/BayesPGLS/blob/master/PGLS.py #
 # Same as Will Pearse's code I think ... ##
 
+## Note to Lizzie: my official version of this file is at #
+# ~/Documents/git/teaching/stan/phylogeny ##
+
+## Note to all: For some reason I had to use an old version of stan/pgls_lemoine.stan to get this to run ##
+
 library(phytools)
+library(rstan)
+library("shinystan")
 
-setwd("~/Documents/git/teaching/stan/phylogeny")
-shorebirdTraits <- read.csv('input/shorebirdData.csv')
+setwd("~/Documents/git/teaching/gelmanhill/BayesPGLS")
+shorebirdTraits <- read.csv('input/shorebirdData.csv', header=TRUE, row.names=1)
+shorebirdVCVdat <- read.csv('input/shorebirdVCV.csv', skip=1, header=FALSE)
+shorebirdVCV <- as.matrix(shorebirdVCVdat)
 
-## START here: Need to update below .... ##
+Lmat <- matrix(rep(1), nrow = nrow(shorebirdVCV), ncol = ncol(shorebirdVCV))
+diag(Lmat) <- 0
 
-shorebirdVCV <- np.array(pd.read_csv('~/Documents/CSU/iDiv/BayesPGLS/shorebirdVCV.csv'))
-Lmat = np.ones(shorebirdVCV.shape)
-Lmat[np.diag_indices(shorebirdVCV.shape[0])] = 0
+stdX <- scale(shorebirdTraits$M.Mass, center=TRUE, scale=TRUE)
+stdY <- scale(shorebirdTraits$Egg.Mass, center=TRUE, scale=TRUE)
 
-compMod = pyst.StanModel(model_code=stanmodel, model_name='TEST')
+## build up data for stan model
+willX <- matrix(rep(1), nrow=nrow(shorebirdTraits), ncol=(length(stdX)))
+N <- nrow(shorebirdTraits)
+X <- as.vector(stdX)
+K <- 1
+V <- shorebirdVCV
+y <- as.vector(stdY)
 
-stdX = (np.log(shorebirdTraits['M.Mass']) - np.log(shorebirdTraits['M.Mass']).mean()) / np.log(shorebirdTraits['M.Mass']).std()
-stdY = (np.log(shorebirdTraits['Egg.Mass']) - np.log(shorebirdTraits['Egg.Mass']).mean()) / np.log(shorebirdTraits['Egg.Mass']).std()
+fit.pgls <- stan("stan/pgls_lemoine.stan", data=c("N","X", "K", "Lmat", "V", "y"), iter=2000, chains=4)
+launch_shinystan(fit.pgls)
 
-X = np.vstack((np.ones(len(shorebirdTraits)), stdX))
-# y = np.log(shorebirdTraits['Egg.Mass'])
+#
+library(caper)
+data(shorebird)
+shorebird <- comparative.data(shorebird.tree, shorebird.data, Species, vcv=TRUE, vcv.dim=3)
+caper.mod <- pgls(scale(Egg.Mass) ~ scale(M.Mass), data=shorebird, lambda='ML')
 
-data = {'N': len(shorebirdTraits), 'X': 
+summary(fit.pgls)
