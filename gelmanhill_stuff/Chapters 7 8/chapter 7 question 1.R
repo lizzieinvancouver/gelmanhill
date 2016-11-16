d0224c33a41ed509fff8e5f9b54b3b659c6d0cb6
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2016-11-16 08:14:29 -0500

    new version shows my mistakes

--- gelmanhill_stuff/Chapters 7 8/chapter 7 question 1.R
+++ gelmanhill_stuff/Chapters 7 8/chapter 7 question 1.R
@@ -3,7 +3,7 @@
 # 1. Discrete prob simlation: 
 
 shots <- function(){
-  xx <- rbinom(2, 1, .6)
+  xx <- rbinom(2, 1, .6) # should this not be xx<- rbinom(2, 1, .4)?
   count = 2
   while(!all(xx[c(count, count-1)]==0)) {
     xx <- c(xx, rbinom(1, 1, .6))
@@ -16,3 +16,22 @@ simz <- replicate(1000, shots())
 mean(simz); sd(simz)
 hist(simz)
 plot(simz)
+
+# In an attempt to answer my own question, I see what happens...
+# In first response, using 0.6 for the probabilty, the simulation the likelihood of
+# how many shots the shooter can make before missing 2 in a row
+# In this answer, taketwo, the shooter is not as good. 
+taketwo<- function() {
+  miss<- rbinom(2, 1, .4)
+  tally = 2
+  while(!all(miss[c(tally, tally-1)]==0)) {
+    miss<- c(miss, rbinom(1, 1, .4))
+    tally = tally + 1
+  }
+  length(miss)
+}
+
+n.sims<-replicate(1000, taketwo())
+mean(n.sims); sd(n.sims)
+hist(n.sims)
+plot(n.sims)
