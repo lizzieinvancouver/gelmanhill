1b33caf48bc64308d4b87bc7be17f34e81ed49bb
MoralesCastilla <moralescastilla@g.harvard.edu>
2017-02-15 10:31:42 -0500

    updated exercise 12.4

--- gelmanhill_stuff/Chapter 11to12/Nacho_Script_Ch.12_exercise4.R
+++ gelmanhill_stuff/Chapter 11to12/Nacho_Script_Ch.12_exercise4.R
@@ -7,33 +7,32 @@
 #' 
 #' Excercise 12.4
 #' 
-
-setwd("C:/Users/Ignacio/Documents/MEGA/Work_Harvard_postdoc/lab meetings/Stanleyi/exercises")
-setwd("C:/Users/Ignacio/Documents/GitHub/gelmanhill/gelmanhill_stuff/Book_Codes/Ch.12")
-
-cd4<-read.csv("allvar.csv")
-head(cd4)
-
-## Exercise 12.2
-# a)Write a model predicting CD4 percentage as a function of time with varying
-# intercepts across children. Fit using lmer() and interpret the coefficient for
-# time.
-
 library(arm)
-cd4$VDATE<-as.Date(cd4$VDATE,format="%m/%d/%Y")
-mod1 <- lmer(CD4PCT ~ VDATE +(1|newpid), data=cd4)
-with(cd4,plot(baseage,CD4PCT))
-confint(mod1, level = 0.99)
+library(ggplot2)
+#setwd("~/MEGA/Work_Harvard_postdoc/lab meetings/Stanleyi/exercises")
+setwd("~/GitHub/gelmanhill/gelmanhill_stuff/Book_Codes/Ch.12")
+
+d<-read.csv("allvar.csv")
+d$time <- d$visage - d$baseage
+d$sqrt<-sqrt(d$CD4PCT)
+d<- d[-which(is.na(d$time)),]
+d<- d[-which(is.na(d$sqrt)),]
+
+
+## model from
+m<- lmer(sqrt~time + (1 + time | newpid), data=d)
+mod <- lm (sqrt ~ 1 + time*newpid, data=d) 
+
+plot(m)
+display(m)
+summary(m)
+
 
 ## Exercise 12.4
-# a) Expand the model including treatment and age
-mod2 <- lmer(CD4PCT ~ treatmnt+baseage +(1|VDATE), data=cd4)
+# 12.2b) Expand the model including treatment and age
+mod2 <- lmer(sqrt ~ time + treatmnt + baseage + (1 | newpid), data=d)
+display(mod2)
 
-#Another common need is to extract the residual standard error, which is necessary for calculating effect sizes. To get a named vector of the residual standard error:
-sigma(mod2)
-
-## effect sizes
-fixef(mod2) / sigma(mod2)
 
 #4. Posterior predictive checking: continuing the previous exercise, use the fitted
 #model from Exercise 12.2(b) to simulate a new dataset of CD4 percentages (with
@@ -42,39 +41,40 @@ fixef(mod2) / sigma(mod2)
 #process 1000 times and compare the simulated distribution to the observed CD4
 #percentage at the final time point for the actual data.
 
-last.time.point<-max(sort(cd4$VDATE))
-display (mod2)
-n.sims <- 1000
-#M2 <- lmer (y ~ x + u.full + (1 | county))
-a.hat.M2 <- fixef(mod2)[1] +  fixef(mod2)[3]*cd4$treatmnt + fixef(mod2)[4]*cd4$baseage+ ranef(mod2)$newpid
+#y.tilde<- #wanted
+mod2 <- lmer(sqrt ~ time + treatmnt + baseage + (1 | newpid), data=d)
+display(mod2)
+
+#generate dataset to store results
+new.d<-data.frame(y.tilde=rep(NA,nrow(d)),d$visage,d$baseage)
+
+#simulate data for final time point
+final.time<-max(d$time)
+#final.time<-max(d$VDATE)
+which.id<-d[which(d$time==final.time),"newpid"]
+treatmnt<-d$treatmnt
+baseage<-d$baseage
+
+a.hat.M2 <- fixef(mod2)[1] + fixef(mod2)[3]*treatmnt + fixef(mod2)[3]*baseage + ranef(mod2)$newpid
 b.hat.M2 <- fixef(mod2)[2]
 
-x.tilde <- last.time.point
+set.seed(123)
+x.tilde <- final.time
 sigma.y.hat <- sigma.hat(mod2)$sigma$data
-coef.hat <- as.matrix (coef(mod2)$VDATE)[511,]
-y.tilde <- rnorm (1, coef.hat %*% c(1, x.tilde, u[26]), sigma.y.hat)
+coef.hat <- as.matrix (coef(mod2)$newpid)[which.id,]
+y.tilde <- rnorm (nrow(d), coef.hat %*% c(1, x.tilde, d$treatmnt[which.id],d$baseage[which.id]), sigma.y.hat)
+mean(y.tilde) #3.865065
+
+
+## repeat and compare posterior distribution
 n.sims <- 1000
-y.tilde <- rnorm (n.sims, coef.hat %*% c(1, x.tilde, u[26]), sigma.y.hat)
-
-quantile (y.tilde, c(.25, .5, .75))
-
-unlogged <- exp(y.tilde)
-mean(unlogged)
-
-
-
-
-posterior<-array(NA,dim=c(1000,1))
-last.time.point<-max(sort(cd4$VDATE))
-cd4.sim<-subset(cd4,VDATE==last.time.point)
-
-for(i in 1:nrow(posterior)){
-  print(i)
-posterior.i<-predict(MLexamp2,newdata=cd4.sim)
-posterior[i]<-mean(posterior.i)
+sims.array<-array(NA,dim=c(nrow(d),n.sims))
+for(i in 1:n.sims){
+print(i)
+    sims.array[,i]<-rnorm (nrow(d), coef.hat %*% c(1, x.tilde, d$treatmnt[which.id],d$baseage[which.id]), sigma.y.hat)
 }
-summary(MLexamp2)
-## plot result
-par(mfrow=c(1,1))
-hist(posterior)
 
+hist(colMeans(sims.array),30,xlim=c(3.7,4.3),col="black")
+abline(v=d[which.id,"sqrt"],col="red")
+
+## I may have got it wrong, still not 100% sure what he refers by final time point
