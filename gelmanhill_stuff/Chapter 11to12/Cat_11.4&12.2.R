a9107429157eabf558850d1a1b08a06eca7bafb9
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2017-02-20 12:12:14 -0500

    Some minor work by Lizzie on Chapter 12 Q2
    And an example of comparing pooling from Heather Kharouba!

--- gelmanhill_stuff/Chapter 11to12/Cat_11.4&12.2.R
+++ gelmanhill_stuff/Chapter 11to12/Cat_11.4&12.2.R
@@ -68,12 +68,20 @@ base<-d[,9]
 patient<-as.factor(d[,2])
 percent<-d[,11]
 time<-d[,10]
-MO<- lmer(percent~time + (1 | patient))
+MO <- lmer(percent~time + (1 | patient))
+
+# be careful doing a lot of indexing with numbers ...
+# it's okay, but safer to use column names
+# so here's an alternative of above that uses data argument:
+MO <- lmer(sqrt~time + (1|patient), data=d)
+
 c.MO<-coef(MO)
 summary(MO)
+
 # 2b - is it just including all predictors then? 
-MO1<- lmer(percent~time + (1+tx | patient) + (1+base | patient))
-MO1<- lmer(percent~time + tx + base + (1|patient))
+MO1 <- lmer(percent~time + (1+tx|newpid) + (1+baseage|newpid))
+# alternative of above that uses data argument:
+MO1<- lmer(sqrt~time + treatmnt + base + (1|patient), data=d)
 display(MO1); display(MO)
 c.MO1<-coef(MO1)
 summary(MO1)
@@ -94,6 +102,22 @@ title("Partial Pooling")
 dev.new()
 plot(coef(MO)$patient[,1], coef(MO1)$patient[,1])
 abline(0,1, col="blue")
+abline(lm(ranef(MO)$patient[,1]~ ranef(MO1)$patient[,1]), col="red")
 
+# a few more ways to plot this ....
+par(mfrow=c(2,1))
+hist(coef(MO)$patient[,1],)
+hist(coef(MO1)$patient[,1],)  # hmm, pretty similar
+
+n_patient <- aggregate(d["VISIT"], d["newpid"], FUN=length)
+plot(n_patient$VISIT, coef(MO)$patient[,1])
+plot(n_patient$VISIT, coef(MO1)$patient[,1])
+
+# If you want to plot with error bars (aka, the way it looks in the book see Book_Code/Ch. 12/12.2_Partial pooling with no predictors.R
+# But! I noticed this works off BUGS code (not lmer code) and so I am not sure how to get the SD used (e.g., see line 78)
+
+## Also, query! What is diff between (they are all adjusted by some value but I am not sure which one!):
+coef(MO)$patient[,1]
+ranef(MO)$patient[,1]
 
 # 2d - They all seem the same... more information for part c but ultimate results?
