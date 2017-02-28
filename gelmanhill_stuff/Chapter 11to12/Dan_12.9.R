8f6228d56792550af3f2ac063281c3aaf1b4e122
dbuona <dbuonaiuto@g.harvard.edu>
2017-02-28 16:56:05 -0500

    Updated 12.9 code
    changed the response variable

--- gelmanhill_stuff/Chapter 11to12/Dan_12.9.R
+++ gelmanhill_stuff/Chapter 11to12/Dan_12.9.R
@@ -8,28 +8,24 @@ setwd("~/Documents/git/gelmanhill/gelmanhill_stuff/ARM_Data/radon")
 ###not sure what these data are
 cty_level<-read.csv(file = "cty.dat")
 ind_level<-read.csv("srrs2.dat")
-###county names?
-countyA<-cty_level$cty
-countyB<-ind_level$county
-setdiff(countyA,countyB)
-intersect(countyA,countyB)
-#no sttfips +cntyfips
+ind_level$log.radon <- log (ifelse (ind_level$activity==0, .1, ind_level$activity))
+
+
 cty_level<-unite(cty_level,fips , c( stfips,ctfips), remove=FALSE)
 ind_level<-unite(ind_level,fips,c(stfips,cntyfips), remove=FALSE)
 ind_level<-left_join(ind_level,cty_level,by= "fips")
 ### okay now there is a column that agrees between datasheet
 
-###now for the actual modelL what is my respose variable  page 267
-##do i need this? u.full<-Uppm[fips]
-fullMod<-lmer(adjwt~floor+ Uppm + (1|fips), data=ind_level)
+fullMod<-lmer(log.radon~floor+ Uppm + (1|fips), data=ind_level)
 display(fullMod)
 
 #1/5subset and repeat
 sub<-sample_frac(ind_level,0.2, replace=TRUE)
-subMod<-lmer(adjwt~floor+ Uppm + (1|fips), data=sub)
+subMod<-lmer(log.radon~floor+ Uppm + (1|fips), data=sub)
 display(subMod)
 ### cluster sample (work around) I know the r code isnt right but maybe still useful
 468/5
-samp <- ind_level[ sample(NROW(ind_level$fips), 93),  ]
-newsubMod<-lmer(adjwt~floor+ Uppm + (1|fips), data=samp)
+onefifth<-sample_frac(cty_level, 0.2, replace = FALSE)
+samp<-semi_join(ind_level, onefifth, by = "fips")
+newsubMod<-lmer(log.radon~floor+ Uppm + (1|fips), data=samp)
 display(newsubMod)
