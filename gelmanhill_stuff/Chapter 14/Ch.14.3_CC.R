88b65621113e6b298baaa96a41ddbeff248c825e
cchambe12 <cchamberlain@g.harvard.edu>
2017-03-14 13:09:34 -0400

    added 14.3 solution attempt

---
+++ gelmanhill_stuff/Chapter 14/Ch.14.3_CC.R
@@ -0,0 +1,34 @@
+# 14 March 2017 - Cat
+## Gelman-Hill Ch 14 Exercise 3
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(arm)
+
+# Set Working Directory
+setwd("~/Documents/git/gelmanhill/gelmanhill_stuff/ARM_Data/rodents")
+d<-read.csv("hvs02_sorted.csv", header=TRUE)
+
+# 14.3a
+# decided to use 'housing' as building separator between community districts
+mod<-glmer(rodent2~old+race+(1|housing),data=d, family=binomial(link="logit"))
+display(mod)
+
+#14.3b - is cd community?
+mod1<-glmer(rodent2~old+race+(1+housing|cd), data=d, family=binomial(link="logit"))
+display(mod1)
+
+ggplot((d), aes(x=housing, y=rodent2)) + xlab("housing") + ylab("rodent2") +
+  geom_point(aes(col=as.factor(cd))) + 
+  geom_smooth(aes(col=as.factor(cd)),method="lm", se=FALSE)
+
+#14.3c
+display(mod);display(mod1) # remember to divide coefficients by 4 for logit models
+coef(mod);coef(mod1)
